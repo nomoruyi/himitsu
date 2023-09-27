5622db9d03df041b5f3df41afaef9649ffcda07c
nomoruyi <nosakhare_omoruyi@hotmail.de>
2023-09-27 08:27:52 +0200

    save

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,9 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:himitsu_app/app/appWidgets('Counter increments smoke test', (WidgetTester tester) async {
+import 'package:himitsu_app/main.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const Himitsu());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
